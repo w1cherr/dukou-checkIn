09d64f5fd364ee788678ea3d469d0c7070ba2b34
w1tcher <>
2023-02-23 12:59:54 +0800

    update: 自动登录

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import axios from "axios"
 
-const token = process.argv[2]
-const url = 'https://dukou.dev/api/user/checkin'
+// const token = process.argv[2]
+const email = process.argv[2]
+const passwd = process.argv[3]
+let token = ''
+const getTokenUrl = 'https://dukou.dev/api/token'
+const checkInUrl = 'https://dukou.dev/api/user/checkin'
+
+async function getToken(){
+    let res;
+	try{
+            res  = await axios.post(getTokenUrl,{
+                email: email,
+                passwd: passwd
+            })
+            if (res.data.token) {
+                res = res.data.token
+            } else {
+                res = '登录失败'
+            }
+	}catch(err){
+            console.log('getTokenErr', err);
+	}
+	return res;
+}
+
 async function checkIn(){
     let res;
 	try{
-            res  = await axios.get(url,{
+            res  = await axios.get(checkInUrl,{
                     headers:{
                             "access-token": token
                     }
@@ -16,14 +39,20 @@ async function checkIn(){
                 res = 'token失效了'
             }
 	}catch(err){
-            console.log(err);
+            console.log('checkInErr', err);
 	}
 	return res;
 }
 
 (async () => {
-    console.log('开始 token:'), token;
-    let result = await checkIn();
-    console.log('result', result);
+    console.log('开始获取token');
+    token = await getToken();
+    console.log('获取token result', token);
+    await new Promise(r => setTimeout(() => {r()}, 1000))
+    if (!!token) {
+        console.log('开始checkIn result');
+        let result = await checkIn();
+        console.log('checkIn result', result);
+    }
     console.log('结束');
   })();
